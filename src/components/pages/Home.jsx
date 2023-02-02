0eaa3ce8b1a2b99f733ff0ad7589fa475be08164
INES Thomas <tines@bimm.com>
2023-02-01 21:49:26 -0500

    add reset data feat

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
-const Home = () => <h2>Home</h2>;
+const Home = () => {
+  const handleResetData = () => {
+    window.localStorage.setItem('students', '[]');
+    window.localStorage.setItem('courses', '[]');
+    window.localStorage.setItem('results', '[]');
+    NotificationManager.success('Data erased successfully');
+  };
+
+  return (
+    <>
+      <h2>Home</h2>
+      <button type="button" onClick={handleResetData}>Reset data</button>
+      <NotificationContainer />
+    </>
+  );
+};
 
 export default Home;
